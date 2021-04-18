085ad881311f8240c2944ec6b753c83c97f8cbaa
Leonard Matheus <13519215@std.stei.itb.ac.id>
2021-04-18 07:46:09 +0000

    Update test_master.py

--- src/test_master.py
+++ src/test_master.py
@@ -1,5 +1,18 @@
 import pytest
 import resep
+import Shopper
+
+#Test Shopper
+
+def test_init_shopperpage():
+    Shopper.shopperpage.__init__(self=Shopper.shopperpage, username="aslan")
+    assert (Shopper.shopperpage.username == "aslan")
+
+def test_init_riwayat():
+    Shopper.riwayat.__init__(self=Shopper.riwayat, usernameInput="aslan2")
+    assert (Shopper.riwayat.username == "aslan2")
+
+#Test Resep
 
 def test_count_resep():
     query = "nasi goreng"
