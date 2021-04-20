99722437a3275f3793fc55f19c32a51144d1e054
leomatt547 <leo.matt.547@gmail.com>
2021-04-20 22:52:09 +0700

    push test case

--- src/test_master.py
+++ src/test_master.py
@@ -4,12 +4,8 @@ import Shopper
 
 #Test Shopper
 
-def test_init_shopperpage():
-    Shopper.shopperpage.__init__(self=Shopper.shopperpage, username="aslan")
-    assert (Shopper.shopperpage.username == "aslan")
-
 def test_init_riwayat():
-    Shopper.riwayat.__init__(self=Shopper.riwayat, usernameInput="aslan2")
+    Shopper.riwayat.__init__(Shopper.riwayat, usernameInput="aslan2")
     assert (Shopper.riwayat.username == "aslan2")
 
 #Test Resep
